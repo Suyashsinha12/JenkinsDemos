1bb2d52ec8df71cd7e39cd9a00ec70f511b46d4f
suyash kumari <suyashsinha12@gmail.com>
2020-11-09 12:07:59 +0530

    first commit

---
+++ ForGIT/src/main/java/com/git/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.git.model.User;
+
+@RestController
+public class WebController {
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		
+		return "Hello world";
+	}
+}
